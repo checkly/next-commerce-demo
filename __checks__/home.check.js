153694368e6e7399c54592aaaf7fc37bffa3d384
Umut Uzgur <umutuzgur@gmail.com>
2022-08-02 15:56:46 +0200

    Update home.check.js

--- __checks__/home.check.js
+++ __checks__/home.check.js
@@ -8,7 +8,6 @@ async function run() {
   const title = await page.title()
   expect(title).toEqual("ACME Storefront | Powered by Next.js Commerce")
   await page.screenshot({ path: 'home_page.jpg' })
-  console.log('screenshot taken')
 
   await cleanUp(page, browser)
 }
